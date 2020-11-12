a640417d78e92895e10201c152d7fd6e45b39fbb
Li Yunfan <TheFoggyCity@live.cn>
2020-11-12 22:50:28 +0800

    Added misc. files

---
+++ stvp_prog.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\STMicroelectronics\st_toolset\stvp\STVP_CmdLine.exe" -BoardName=ST-LINK -Port=USB -ProgMode=SWIM -Device=STM8S103F3 -verif -no_loop -no_log -FileProg=build\main.ihx
